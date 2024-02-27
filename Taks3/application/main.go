c94dc97a504037f7296a9e14a0b012894cdc211f
Rameshwar-Kanade <rameshwar.kanade@veltris.com>
2024-02-27 09:30:29 +0000

    Deploy pods on the cluster using client

---
+++ Taks3/application/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	// "github.com/Rameshwar-Kanade/application/go/pkg/mod/k8s.io/client-go@v0.29.2/rest"
+	// "github.com/Rameshwar-Kanade/application/go/pkg/mod/k8s.io/client-go@v0.29.2/tools/clientcmd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := flag.String("kubeconfig", "/home/ubuntu/.kube/config", "location to your kubeconfig file")
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		fmt.Printf("error %s building config from flags\n", err.Error())
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			fmt.Printf("error %s, getting inclusterconfig\n", err.Error())
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("error %s, creating clientset\n", err.Error())
+	}
+
+	ctx := context.Background()
+
+	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("error %s, while listing all the pods from default namespace\n", err.Error())
+	}
+
+	fmt.Println("Pods from default namespace")
+	for _, pod := range pods.Items {
+		fmt.Printf("%+v\n", pod)
+	}
+
+	fmt.Println("Deployments are")
+	deployments, err := clientset.AppsV1().Deployments("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("listing deployments %s\n", err.Error())
+	}
+
+	for _, d := range deployments.Items {
+		fmt.Printf("%s\n", d.Name)
+	}
+}
